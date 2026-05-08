520c2def97c377445311c6a213f2321a0485a314
Himanshu121865 <himanshujha1218@gmail.com>
2026-05-08 20:25:23 +0530

    dark mode

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss from "astrojs/rss";
+
+export async function GET(context) {
+  return rss({
+    title: "Astro Learner | Blog",
+    description: "My journey learning Astro",
+    site: context.site,
+    item: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    customData: `<language>en-us</language>`,
+  });
+}
